159647ac8df3a2e5004dd2db134952334cbdb1a2
Felipe Valtl de Mello <valtlfelipe@gmail.com>
2025-08-20 09:34:26 -0300

    fix: add user-agent to GH request

--- server/api/latest-release.ts
+++ server/api/latest-release.ts
@@ -1,5 +1,9 @@
 export default defineCachedEventHandler(async (event) => {
-  const response = await $fetch(`https://api.github.com/repos/valtlfelipe/hedit/releases/latest`)
+  const response = await $fetch(`https://api.github.com/repos/valtlfelipe/hedit/releases/latest`, {
+    headers: {
+      'User-Agent': 'hedit.app website',
+    },
+  })
   return response
 }, {
   maxAge: 60 * 60, // 1 hour
